fa9f00ba228fe4b921c42a2709d8f8352d8f56c8
MoazMahmoud11 <moazmahmoud1182003@gmail.com>
2026-06-17 02:52:11 +0300

    Adding some Responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import React, { Suspense } from 'react';
+import React, { Suspense } from "react";
 
 import Cart from "./Components/Cart.jsx";
-const Checkout = React.lazy(() => import('./Components/Checkout.jsx'));
 import Header from "./Components/Header.jsx";
-const OrdersArea = React.lazy(() => import('./Components/OrdersArea.jsx'));
-import {CartContextProvider} from "./store/CartContext.jsx";
-import {UserProgressContextProvider} from "./store/UserProgressContext.jsx";
+import { CartContextProvider } from "./store/CartContext.jsx";
+import { UserProgressContextProvider } from "./store/UserProgressContext.jsx";
+const Checkout = React.lazy(() => import("./Components/Checkout.jsx"));
+const OrdersArea = React.lazy(() => import("./Components/OrdersArea.jsx"));
 
 function App() {
   return (
-      <CartContextProvider >
-        <UserProgressContextProvider>
-          <Header />
-          <Suspense fallback={true}>
-            <OrdersArea />
-          </Suspense>
-          <Cart/>
-          <Suspense fallback={true}>
-            <Checkout />
-          </Suspense>
-        </UserProgressContextProvider>
-      </CartContextProvider>
-  )
+    <CartContextProvider>
+      <UserProgressContextProvider>
+        <Header />
+        <Suspense fallback={true}>
+          <OrdersArea />
+        </Suspense>
+        <Cart />
+        <Suspense fallback={true}>
+          <Checkout />
+        </Suspense>
+      </UserProgressContextProvider>
+    </CartContextProvider>
+  );
 }
 
 export default App;
